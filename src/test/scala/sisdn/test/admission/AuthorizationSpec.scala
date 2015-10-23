7623f09e34bb763586138d2aa9e11195d4572c6d
mhashim <mhashim.gm@gmail.com>
2015-10-23 14:33:56 +0300

    Some refactoring

--- src/test/scala/sisdn/test/admission/AuthorizationSpec.scala
+++ src/test/scala/sisdn/test/admission/AuthorizationSpec.scala
@@ -1,8 +1,22 @@
 package sisdn.test.admission
 
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.RequestContext
 import org.scalatest.{FlatSpec, Matchers}
+import headers._
 import sisdn.admission.utils._
 
+import scala.concurrent.Promise
+
 class AuthorizationSpec extends FlatSpec with Matchers {
+//  object ctx extends RequestContext(HttpRequest(
+//    HttpMethod.custom(""),
+//    Uri(),
+//    Seq(Authorization(OAuth2BearerToken("")))
+//  ), Promise.successful(HttpResponse), 5){
+//    val request = HttpRequest
+//
+//  }
 
 }
